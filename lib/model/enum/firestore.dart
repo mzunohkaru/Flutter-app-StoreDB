8156bad8fce2b08a381f96a202a61a40f18e4c80
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-01-07 14:52:53 +0900

    add:genre prop

--- lib/model/enum/firestore.dart
+++ lib/model/enum/firestore.dart
@@ -7,19 +7,25 @@ enum Genre {
   business,
   socialNetworking,
 	healthAndFitness,
+	lifeStyle,
+  travel,
 }
 
 extension GenreExtension on Genre {
   int get code => {
     Genre.business: 6000,
     Genre.socialNetworking: 6005,
-    Genre.healthAndFitness: 6012,
+    Genre.healthAndFitness: 6013,
+    Genre.lifeStyle: 6012,
+    Genre.travel: 6003,
   }[this]!;
 
   String get title => {
     Genre.business: 'ビジネス',
     Genre.socialNetworking: 'ソーシャルネットワーキング',
     Genre.healthAndFitness: 'ヘルス＆フィットネス',
+    Genre.lifeStyle: 'ライフスタイル',
+    Genre.travel: 'トラベル',
   }[this]!;
 }
 
