17921c1095b050fe9a4175a419cfbde2b99e6248
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-01-07 15:38:07 +0900

    style:refactor

---
+++ lib/ui/app_list/app_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../model/enum/firestore.dart';
+import 'widget/app_list_widget.dart';
+
+class AppListScreen extends StatelessWidget {
+  const AppListScreen({super.key, required this.genre});
+  final Genre genre;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(genre.title),
+      ),
+      body: AppListWidget(
+        genre: genre,
+        onTap: (app) {
+          context.go(
+            '/appList/${genre.name}/chart',
+            extra: app,
+          );
+        },
+      ),
+    );
+  }
+}
